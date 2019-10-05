f9bd6f552533ae71968ecf59c47803a505066df4
kinosstik <56196038+kinosstik@users.noreply.github.com>
2019-10-05 17:01:49 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -33,21 +33,21 @@ $('html, body').stop().animate({
  });
 
 
-$(document).ready(function () {
-        $("#callback").submit(function (e) { // Устанавливаем событие отправки для формы с id=form
-            e.preventDefault();
-            var form_data = $(this).serialize(); // Собираем все данные из формы
-            $.ajax({
-                type: "POST", // Метод отправки
-                url: "send.php", // Путь до php файла отправителя
-                data: form_data,
-                success: function () {
-                    // Код в этом блоке выполняется при успешной отправке сообщения
-                    alert("Ваше сообщение отправлено!");
-                }
-            });
-        });
-    });    
+// $(document).ready(function () {
+//         $("#callback").submit(function (e) { // Устанавливаем событие отправки для формы с id=form
+//             e.preventDefault();
+//             var form_data = $(this).serialize(); // Собираем все данные из формы
+//             $.ajax({
+//                 type: "POST", // Метод отправки
+//                 url: "send.php", // Путь до php файла отправителя
+//                 data: form_data,
+//                 success: function () {
+//                     // Код в этом блоке выполняется при успешной отправке сообщения
+//                     alert("Ваше сообщение отправлено!");
+//                 }
+//             });
+//         });
+//     });    
 
 
 $(function(){
